0b281d6cd8a58fb8caa220bf4a66591a8677bccb
vizunia97 <vincenth97@proton.me>
2025-04-23 09:07:12 -0600

    Update type_constants.asm
    ADD SOUND AND LIGHT

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -32,6 +32,8 @@ DEF SPECIAL EQU const_value
 	const ICE
 	const DRAGON
 	const DARK
+ const SOUND
+ const LIGHT
 DEF TYPES_END EQU const_value
 
 DEF NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END - 1 ; discount BIRD
